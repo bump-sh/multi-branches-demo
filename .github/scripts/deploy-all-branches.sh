c7a38d17392eed64e92732482012ffecea8a5eb5
Polo M2B <polo2bodi@gmail.com>
2025-07-11 10:33:52 +0200

    Add GitHub action scripts
    GitHub workflow is called when commit is merged on 'main',
    which generate call to script deploy-all-branches.sh
    
    In this script, we assume doc structure is
    doc/{branchName}/openapi.yml,
    this can of course be customized.
    
    Each folder has its own branch on Bump.sh documentation:
    
    https://bump.sh/demo/doc/multi-branches-demo

---
+++ .github/scripts/deploy-all-branches.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Don't return null result on glob patterns
+shopt -s nullglob
+
+# Install bump-cli
+npm install -g bump-cli
+
+apiKey="BUMP_TOKEN"
+docId="BUMP_DOC_ID"
+
+echo "Using token ${apiKey} for Documentation 'multi-branches-demo' (id: ${!docId})"
+
+# For each *-source.yaml files in the apis/ directory
+for api in doc/*/openapi.{yml,yaml,json}; do
+    [ -f "${api}" ] || continue
+    branchName=$(basename "$(dirname "$api")")
+    echo "Deploying ${api} on branch ${branchName}"
+
+    bump deploy --doc "${docId}" --token "${!apiKey}" --branch "${branchName}" "${api}"
+done
--- README.md
+++ README.md
@@ -3,3 +3,17 @@
 Purpose of this repo is to implement a script
 with Bump.sh GitHub action, to deploy several APIs
 to specific branches.
+
+
+## GitHub action scripts
+
+GitHub workflow is called when commit is merged on 'main',
+which generate call to script deploy-all-branches.sh
+
+In this script, we assume doc structure is
+doc/{branchName}/openapi.yml,
+this can of course be customized.
+
+Each folder has its own branch on Bump.sh documentation:
+
+https://bump.sh/demo/doc/multi-branches-demo
